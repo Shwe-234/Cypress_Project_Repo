51f5c8da1bb6e1a846c6f851bca311d1e9edf3c6
Shwe-234 <107454206+Shwe-234@users.noreply.github.com>
2022-07-08 08:15:14 +0300

    Update CypressStudio.js

--- cypress/integration/tests/CypressStudio.js
+++ cypress/integration/tests/CypressStudio.js
@@ -2,7 +2,6 @@
 describe("Test login functionality",()=>{
     it("Open URL",()=>{
         cy.visit("https://www.saucedemo.com/")
-        /* ==== Generated with Cypress Studio ==== */
         cy.get('[data-test="username"]').clear();
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').clear();
